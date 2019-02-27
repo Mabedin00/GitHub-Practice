757e3ef337e72029fb262de3d582c8ff6c04aa84
Mohidul Abedin <mabedin00@stuy.edu>
2019-02-26 19:31:51 -0500

    first commit

---
+++ HelloWorld.java
@@ -0,0 +1,29 @@
+/*****************************************
+ *  File Name:    HelloWorld.java
+ *  Compilation:  javac HelloWorld.java
+ *  Execution:    java HelloWorld
+ *
+ *  Prints "hello, world". By tradition,
+ *  this is everyone's first program.
+
+
+ javac -version
+javac 1.7.0_67
+
+java -version
+java version "10.0.2" 2018-07-17
+Java(TM) SE Runtime Environment 18.3 (build 10.0.2+13)
+Java HotSpot(TM) 64-Bit Server VM 18.3 (build 10.0.2+13, mixed mode)
+
+0. .7 hours
+1. The hardest part was determining whether or not my javac was the updated version. I gave up on it since it works fine with my code.
+2.
+    >hello, world. Wow java is so fun.
+ *****************************************/
+
+ public class HelloWorld{
+
+   public static void main (String[] commandline) {
+     System.out.println("hello, world. Wow java is so fun.");
+   }
+ }
